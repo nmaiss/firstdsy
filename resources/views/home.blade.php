1b9b7561f8cbb9d293e5e818b9a960b227eadb1b
nmaiss <nicolas.maissouradze@gmail.com>
2021-04-20 01:06:22 +0300

    f

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -76,14 +76,16 @@
 
 @section('content')
 <div class="logout-button">
-    <a href="{{ route('logout') }}"
-       onclick="event.preventDefault();
-                     document.getElementById('logout-form').submit();">
-        <img src="{{ URL::to('/assets/img/Deconnexion.png') }}">
-    </a>
-    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-        @csrf
-    </form>
+    <div class="logout">
+        <a href="{{ route('logout') }}"
+           onclick="event.preventDefault();
+                         document.getElementById('logout-form').submit();">
+            <img src="{{ URL::to('/assets/img/Deconnexion.png') }}">
+        </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+        </form>
+    </div>
 </div>
 <div class="container-fluid">
     <div class="geeks"></div>
